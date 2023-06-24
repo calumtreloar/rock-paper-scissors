b44a1e65e21f2d312c87afcf5941d2faf636e187
calumtreloar <calumtreloar@gmail.com>
2023-06-25 00:03:15 +0930

    add buttons for each choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,12 @@
+const buttons = document.querySelectorAll("button");
 let playerScore = 0;
 let computerScore = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playRound(button.textContent.toLowerCase(), getComputerChoice());
+  });
+});
 
 function getComputerChoice() {
   const computerArray = ["rock", "paper", "scissors"];
@@ -14,49 +21,46 @@ function playRound(playerSelection, computerSelection) {
   // All possible outcomes for a game of rock paper scissors
   if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore++;
+    console.log("Computer wins");
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     playerScore++;
+    console.log("Player wins");
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore++;
+    console.log("Player wins");
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     computerScore++;
+    console.log("Computer wins");
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     computerScore++;
+    console.log("Computer wins");
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore++;
+    console.log("Player wins");
   } else if (playerSelection === "rock" && computerSelection === "rock") {
+    console.log("Its a draw");
     return "It's a draw!";
   } else if (playerSelection === "paper" && computerSelection === "paper") {
+    console.log("Its a draw");
     return "It's a draw!";
-  } else if (
-    playerSelection === "scissors" &&
-    computerSelection === "scissors"
-  ) {
+  } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+    console.log("Its a draw");
     return "It's a draw!";
   }
 }
 
-function game() {
+/* function game() {
   // Loops 5 times
   for (let i = 0; i < 5; i++) {
     // Calls the computer choice function and assigns it to a variable
     const computerSelection = getComputerChoice();
     // Asks the player for their rock, paper, scissors choice & removes case sensitivity by converting to lowercase
-    const playerSelection = prompt(
-      "Please enter either rock, paper or scissors"
-    ).toLowerCase();
+    const playerSelection = prompt("Please enter either rock, paper or scissors").toLowerCase();
     // Plays a round of rock paper scissors
     playRound(playerSelection, computerSelection);
     // Displays how many wins the player and computer have attained
-    console.log(
-      "The player has won ",
-      playerScore,
-      " game!",
-      " The computer has won ",
-      computerScore,
-      " game!"
-    );
-  }
+    console.log("The player has won ", playerScore, " game!", " The computer has won ", computerScore, " game!");
+  } 
 
   if (playerScore > computerScore) {
     return "Player wins!";
@@ -65,6 +69,7 @@ function game() {
   } else {
     return "It was a draw!";
   }
+  
 }
-
-console.log(game());
+*/
+// console.log(game());
